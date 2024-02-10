48a3dd6da8b0bda33540869b4763ec0de46482c0
Cyrill <vpopi4@ya.ru>
2024-02-11 00:22:02 +0300

    added AbctractInMemoryRepository created in memory repository for all entities

---
+++ src/repository/InMemoryEpicRepository.java
@@ -0,0 +1,8 @@
+package repository;
+
+import interfaces.repository.EpicRepository;
+import model.Epic;
+
+public class InMemoryEpicRepository extends AbstractInMemoryRepository<Epic> implements EpicRepository {
+
+}
