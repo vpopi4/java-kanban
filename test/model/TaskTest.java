892bafe215e3abcd41c7268466a5303903404c11
Cyrill <vpopi4@ya.ru>
2024-02-12 02:40:11 +0300

    added unit tests for Task

---
+++ test/model/TaskTest.java
@@ -0,0 +1,57 @@
+package model;
+
+import dtos.TaskCreationData;
+import enums.TaskStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    @Test
+    public void shouldCreateTaskWithTaskCreationData() {
+        TaskCreationData data = new TaskCreationData("Test Task", "Description");
+
+        Task task = new Task(1, data);
+
+        assertEquals(1, task.getId());
+        assertEquals("Test Task", task.getName());
+        assertEquals("Description", task.getDescription());
+        assertEquals(TaskStatus.NEW, task.getStatus());
+    }
+
+    @Test
+    public void shouldReturnExpectedStringRepresentation() {
+        TaskCreationData data = new TaskCreationData("Test Task", "Description");
+        Task task = new Task(1, data);
+
+        String expectedString = "Task{" +
+                "\n\tid=1" +
+                ",\n\tname='Test Task'" +
+                ",\n\tdescription='Description'" +
+                ",\n\tstatus=NEW" +
+                "\n}";
+        String actualString = task.toString();
+
+        assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void shouldBeEqualIfAllPropertiesAreEqual() {
+        TaskCreationData data1 = new TaskCreationData("Test Task", "Description");
+        TaskCreationData data2 = new TaskCreationData("Test Task", "Description");
+        Task task1 = new Task(1, data1);
+        Task task2 = new Task(1, data2);
+
+        assertEquals(task1, task2);
+    }
+
+    @Test
+    public void shouldNotBeEqualIfOnlyIdsAreEqual() {
+        TaskCreationData data1 = new TaskCreationData("Test Task", "Description");
+        TaskCreationData data2 = new TaskCreationData("Test Task", "Different Description");
+        Task task1 = new Task(1, data1);
+        Task task2 = new Task(1, data2);
+
+        assertNotEquals(task1, task2);
+    }
+}
