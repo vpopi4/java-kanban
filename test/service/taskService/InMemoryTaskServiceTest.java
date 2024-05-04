974c51a322a9d1153ae6697986676e25d1d19c9a
vpopi4 <vpopi4@yandex.ru>
2024-05-05 02:53:42 +0300

    refactor: move test file

--- test/service/taskService/InMemoryTaskServiceTest.java
+++ test/service/taskService/InMemoryTaskServiceTest.java
@@ -1,4 +1,4 @@
-package service;
+package service.taskService;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -186,4 +186,35 @@ public class InMemoryTaskServiceTest {
         assertEquals(3, task3.getId());
     }
 
+    @Test
+    public void testUpdation_withoutUpdateMethod() {
+        // Arrange
+        Task task = taskService.create(new TaskCreationData("name", "desk"));
+
+        // Act
+        task.setStatus(TaskStatus.DONE);
+        Task taskFromRepo = taskService.get(0);
+
+        // Assert
+        assertSame(task, taskFromRepo);
+
+        // Arrange
+        taskService.create(new TaskCreationData("name1", "some text"));
+
+        // Act
+        Task task1 = taskService.get(1);
+
+        task1.setStatus(TaskStatus.DONE);
+
+        Task task1_ = taskService.get(1);
+
+        // Assert
+        assertSame(task1_, task1);
+        assertEquals(TaskStatus.DONE, task1_.getStatus());
+
+        // update and create methods return link that are used inside the repository by the HashMap,
+        // so actually we can update task without calling update method
+        // probably we need fix it
+        // TODO: fix in memory storing
+    }
 }
