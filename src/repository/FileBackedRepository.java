ddf2f1a0e708d4aa4ff3132a794644cc6fc4e22b
vpopi4 <vpopi4@yandex.ru>
2024-06-05 19:08:27 +0300

    feat: add loading from backup file

--- src/repository/FileBackedRepository.java
+++ src/repository/FileBackedRepository.java
@@ -45,4 +45,64 @@ public class FileBackedRepository extends InMemoryRepository {
             throw new ManagerSaveException();
         }
     }
+
+    public void clear() {
+        store.clear();
+        tasks.clear();
+        epics.clear();
+        subtasks.clear();
+    }
+
+    public void clearAndLoadBackup() {
+        try {
+            clear();
+
+            if (!Files.exists(pathToBackup)) {
+                return;
+            }
+
+            String[] data = Files.readString(
+                    pathToBackup,
+                    StandardCharsets.UTF_8
+            ).split("\n");
+
+            for (int i = 1; i < data.length; i++) {
+                Taskable record = TaskableFactory.deserialize(data[i]);
+                backToTheStore(record);
+            }
+
+            restoreRelations();
+        } catch (IOException e) {
+            throw new ManagerSaveException();
+        }
+    }
+
+    private void backToTheStore(Taskable record) {
+        store.put(record.getId(), record);
+
+        if (record.getType() == TaskType.TASK) {
+            tasks.add(record.getId());
+        } else if (record.getType() == TaskType.EPIC) {
+            epics.add(record.getId());
+        } else if (record.getType() == TaskType.SUBTASK) {
+            subtasks.add(record.getId());
+        }
+    }
+
+    private void restoreRelations() {
+        HashMap<Integer, List<Integer>> subtaskIdsOfEpics = new HashMap<>();
+
+        for (Epic epic : getAllEpics()) {
+            List<Integer> subtaskIds = new ArrayList<>();
+
+            subtaskIdsOfEpics.put(epic.getId(), subtaskIds);
+            epic.setSubtaskIds(subtaskIds);
+        }
+
+        for (Subtask subtask : getAllSubtasks()) {
+            subtaskIdsOfEpics
+                    .get(subtask.getEpicId())
+                    .add(subtask.getId());
+        }
+    }
 }
