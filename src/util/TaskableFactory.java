bc71603a28c2b1bd53ff60c56221e1ec3d9f6233
vpopi4 <vpopi4@yandex.ru>
2024-06-08 21:20:14 +0300

    feat: change serialization and deserialization with new fields

--- src/util/TaskableFactory.java
+++ src/util/TaskableFactory.java
@@ -5,17 +5,21 @@ import model.Epic;
 import model.Subtask;
 import model.Task;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 public class TaskableFactory {
     public static String serialize(Taskable entity) {
-        // id,type,name,status,description,epic
+        // id,type,name,status,description,duration,startTime,epic
         String record = entity.getId() + "," +
                 entity.getType() + "," +
                 entity.getName() + "," +
                 entity.getStatus() + "," +
-                entity.getDescription() + ",";
+                entity.getDescription() + "," +
+                entity.getDuration() + "," +
+                entity.getStartTime() + ",";
 
         if (entity.getType() == TaskType.SUBTASK) {
             record += ((Subtask) entity).getEpicId();
@@ -32,26 +36,34 @@ public class TaskableFactory {
         String name = record.get(2);
         TaskStatus status = TaskStatus.valueOf(record.get(3));
         String description = record.get(4);
+        Duration duration = Duration.parse(record.get(5));
+        LocalDateTime startTime = LocalDateTime.parse(record.get(6));
 
         if (type == TaskType.TASK) {
             Task task = new Task(id);
             task.setName(name);
             task.setStatus(status);
             task.setDescription(description);
+            task.setDuration(duration);
+            task.setStartTime(startTime);
             return task;
         } else if (type == TaskType.EPIC) {
             Epic epic = new Epic(id);
             epic.setName(name);
             epic.setStatus(status);
             epic.setDescription(description);
+            epic.setDuration(duration);
+            epic.setStartTime(startTime);
             return epic;
         } else if (type == TaskType.SUBTASK) {
-            Integer epicId = Integer.parseInt(record.get(5));
+            Integer epicId = Integer.parseInt(record.get(7));
 
             Subtask task = new Subtask(id, epicId);
             task.setName(name);
             task.setStatus(status);
             task.setDescription(description);
+            task.setDuration(duration);
+            task.setStartTime(startTime);
             return task;
         }
         throw new IllegalArgumentException("record can not be resolved");
