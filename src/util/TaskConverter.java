e9748fcc2cefb7959e8b6783166698579dcc213c
vpopi4 <vpopi4@yandex.ru>
2024-06-16 05:01:27 +0300

    feat (TaskConverter): add json convertation

--- src/util/TaskConverter.java
+++ src/util/TaskConverter.java
@@ -1,17 +1,26 @@
 package util;
 
+import com.google.gson.*;
 import interfaces.model.Taskable;
 import model.Epic;
 import model.Subtask;
 import model.Task;
 
+import java.lang.reflect.Type;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class TaskConverter {
+    private static final Gson gson = new GsonBuilder()
+            .serializeNulls()
+            .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+            .registerTypeAdapter(Duration.class, new DurationAdapter())
+            .create();
+
     public static String toCsvRecord(Taskable entity) {
         // id,type,name,status,description,duration,startTime,epic
         Duration duration = entity.getDuration() == null
@@ -74,5 +83,57 @@ public class TaskConverter {
             return task;
         }
         throw new IllegalArgumentException("record can not be resolved");
+    }
+
+    public static String toJson(Taskable taskable) {
+        return gson.toJson(taskable);
+    }
+
+    public static Taskable formJson(String json) {
+        Taskable taskable = gson.fromJson(json, Task.class);
+        if (taskable.getType() == TaskType.EPIC) {
+            taskable = gson.fromJson(json, Epic.class);
+        } else if (taskable.getType() == TaskType.SUBTASK) {
+            taskable = gson.fromJson(json, Subtask.class);
+        }
+        return taskable;
+    }
+
+    private static class LocalDateTimeAdapter implements JsonSerializer<LocalDateTime>, JsonDeserializer<LocalDateTime> {
+        private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+
+        @Override
+        public JsonElement serialize(LocalDateTime src,
+                                     Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            return new JsonPrimitive(src.format(formatter));
+        }
+
+        @Override
+        public LocalDateTime deserialize(
+                JsonElement json,
+                Type typeOfT,
+                JsonDeserializationContext context
+        ) throws JsonParseException {
+            return LocalDateTime.parse(json.getAsString(), formatter);
+        }
+    }
+
+    private static class DurationAdapter implements JsonSerializer<Duration>, JsonDeserializer<Duration> {
+        @Override
+        public JsonElement serialize(Duration src,
+                                     Type typeOfSrc,
+                                     JsonSerializationContext context) {
+            return new JsonPrimitive(src.toMillis());
+        }
+
+        @Override
+        public Duration deserialize(
+                JsonElement json,
+                Type typeOfT,
+                JsonDeserializationContext context
+        ) throws JsonParseException {
+            return Duration.ofMillis(json.getAsLong());
+        }
     }
 }
